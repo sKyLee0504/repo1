83c6d620aad4094178246a1f796d698e2ed76fcc
lixm <lxmcf110@163.com>
2022-08-16 21:24:23 +0800

    字符串及其一些方法

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -7,7 +7,8 @@ Console.WriteLine("Hello, World!");
 // 浮点数 double 10.0
 //布尔值 bool true false
 
-string name = "sky";
+string name = "Sky";
+string cent = "helloWorld is sky";
 char sex = 'M';
 int age = 24;
 double height = 184.9;
@@ -21,3 +22,20 @@ Console.WriteLine("是否是男性：" + is_male);
 
 // 字符串换行
 Console.WriteLine("名字：" + "\n" + name);
+Console.WriteLine("\"hello! {0}\"" + "\n" ,name);
+Console.WriteLine(name.Length);
+
+// 字符串内全部大写的方法.ToUpper()
+Console.WriteLine(name.ToUpper());
+// 字符串内全部小写的方法.ToLower()
+Console.WriteLine(name.ToLower());
+// 检测字符串内是否有子字串的方法.Contains(带入需要检测的字符串)
+Console.WriteLine(name.Contains("k"));
+// 返回字符串内哪个位置的值
+Console.WriteLine(name[1]);
+// 返回字符串内哪个值的位置
+Console.WriteLine(name.IndexOf("ky"));
+// 返回字符串内哪个值的位置（找不到返回-1
+Console.WriteLine(name.IndexOf("Sy"));
+// 字符串切割（第二个参数取几位
+Console.WriteLine(cent.Substring(1,5));
