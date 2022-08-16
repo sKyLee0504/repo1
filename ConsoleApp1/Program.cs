af17da612d2d85f66ad141f065ee572d544093f8
lixm <lxmcf110@163.com>
2022-08-16 21:00:23 +0800

    提交修改

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,3 +1,23 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
+// 字符串 string " "
+// 字节 char ''
+// 整型 int 10
+// 浮点数 double 10.0
+//布尔值 bool true false
+
+string name = "sky";
+char sex = 'M';
+int age = 24;
+double height = 184.9;
+bool is_male = true;
+
+Console.WriteLine("名字：" + name);
+Console.WriteLine("性别：" + sex);
+Console.WriteLine("年龄：" + age);
+Console.WriteLine("高度：" + height);
+Console.WriteLine("是否是男性：" + is_male);
+
+// 字符串换行
+Console.WriteLine("名字：" + "\n" + name);
